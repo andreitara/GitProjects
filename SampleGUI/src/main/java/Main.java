8adfa08dd56d261e08203bfc77aa6b2146c94468
andreitara <kantry007@gmail.com>
2014-11-11 23:47:33 +0200

    SampleGUI Project
    create SampleGUI Project

---
+++ SampleGUI/src/main/java/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Andrei on 11/10/2014.
+ */
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("Hello World Maven");
+        new MainGUI();
+    }
+
+}
